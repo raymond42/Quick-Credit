776b9ae336a683fd91e497373a020c19c46014e6
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-04-22 09:02:09 -0400

    [feature #165476891] add API for a user to apply for a loan

---
+++ server/models/loans.js
@@ -0,0 +1,19 @@
+const loans = [];
+
+const loan = {
+  id: 1,
+  createdOn: '01/01/2019',
+  firstname: 'Raymond',
+  lastName: 'Gakwaya',
+  email: 'raymond@gmail.com',
+  status: 'approved',
+  repaid: ' False',
+  tenor: '12 months',
+  amount: 600000,
+  paymentInstallment: 52500,
+  balance: 390000,
+  interest: 30000,
+};
+
+loans.push(loan);
+export default loans;
