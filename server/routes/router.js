643b7ae7f8803bdf86d9b2b660b35a6bfe7c6d60
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-04-24 16:40:34 +0200

    [feature #165476915] add an API for admin to get single loan application

--- server/routes/router.js
+++ server/routes/router.js
@@ -4,7 +4,7 @@ import signin from '../controllers/users/signin';
 import apply from '../controllers/users/apply';
 import auth from '../middleware/auth';
 import getHistory from '../controllers/users/getHistory';
-import getApplications from '../controllers/admin/applications';
+import { getApplications, getOneApplication } from '../controllers/admin/applications';
 
 const router = express.Router();
 
@@ -16,5 +16,6 @@ router.post('/signin', signin);
 router.post('/apply', auth, apply);
 router.get('/history/:id', auth, getHistory);
 router.get('/applications', auth, getApplications);
+router.get('/application/:id', auth, getOneApplication);
 
 export default router;
