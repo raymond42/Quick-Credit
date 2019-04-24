bc1c84f2ce8f39d3df37775574234557ac07cae8
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-04-24 17:04:14 +0200

    [chore #165567453] add unit-tests for getting single loan application api

--- test/getApplications.js
+++ test/getApplications.js
@@ -7,11 +7,13 @@ import app from '../server/app';
 chai.use(chaiHttp);
 chai.should();
 
-describe('View all loan applications', () => {
-  const user = {
-    email: 'ray@gmail.com',
-  };
-  const token = jwt.sign(user, 'SECRET_KEY', { expiresIn: '15min' });
+const user = {
+  email: 'ray@gmail.com',
+};
+const token = jwt.sign(user, 'SECRET_KEY', { expiresIn: '15min' });
+
+// getting all loan applications
+describe('Get all loan applications', () => {
   it('admin should be able to get all loan applications', (done) => {
     chai.request(app)
       .get('/api/v1/admin/applications')
@@ -28,6 +30,47 @@ describe('View all loan applications', () => {
   it('admin should not be able to get any loan application when he is not authorized', (done) => {
     chai.request(app)
       .get('/api/v1/admin/applications')
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.should.be.an('object');
+        res.body.should.have.property('status').eql(401);
+        res.body.should.have.property('error');
+        done();
+      });
+  });
+});
+
+// getting single loan application
+describe('Get a single loan application', () => {
+  it('admin should be able to get a single loan application', (done) => {
+    chai.request(app)
+      .get('/api/v1/admin/application/1')
+      .set('Authorization', token)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.an('object');
+        res.body.should.have.property('status').eql(200);
+        res.body.should.have.property('data');
+        done();
+      });
+  });
+
+  it('admin should not be able to get a loan application when there is none with the requested id', (done) => {
+    chai.request(app)
+      .get('/api/v1/admin/application/1234')
+      .set('Authorization', token)
+      .end((err, res) => {
+        res.should.have.status(404);
+        res.should.be.an('object');
+        res.body.should.have.property('status').eql(404);
+        res.body.should.have.property('error');
+        done();
+      });
+  });
+
+  it('admin should not be able to get a loan application when he/she is not authorized', (done) => {
+    chai.request(app)
+      .get('/api/v1/admin/application/1234')
       .end((err, res) => {
         res.should.have.status(401);
         res.should.be.an('object');
