ac79a08802841cf727cd08953e1f29e9ecd68b24
Raymond Gakwaya <raymond42.gr@gmail.com>
2019-04-26 22:48:17 +0200

    [chore #165649428] add endpoints for heroku

--- server/app.js
+++ server/app.js
@@ -3,15 +3,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import usersRoutes from './routes/users';
 import adminRoutes from './routes/admin';
+import herokuRoutes from './routes/Heroku';
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.json());
 
-// app.use(users);
 app.use('/api/v1/users/', usersRoutes);
 app.use('/api/v1/admin/', adminRoutes);
+
+// for heroku deploymennt
+app.use('/api/v1/heroku/users/', herokuRoutes);
+app.use('/api/v1/heroku/admin/', herokuRoutes);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => { console.log(`app is running on ${port}...`); });
---
+++ server/routes/Heroku.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import signup from '../controllers/users/signup';
+import signin from '../controllers/users/signin';
+import apply from '../controllers/users/apply';
+import getHistory from '../controllers/users/getHistory';
+import { getApplications, getOneApplication } from '../controllers/admin/applications';
+import { getUsers, getOneUser, markUser } from '../controllers/admin/users';
+import getCurrentLoans from '../controllers/admin/current';
+import getRepaidLoans from '../controllers/admin/repaid';
+
+
+const router = express.Router();
+
+// signup
+router.post('/signup', signup);
+
+// signin
+router.post('/signin', signin);
+
+// apply
+router.post('/apply', apply);
+
+// get loan repayment history
+router.get('/history/:id', getHistory);
+
+// get all loan applications
+router.get('/applications', getApplications);
+
+// get single loan application
+router.get('/application/:id', getOneApplication);
+
+// get all users/clients
+router.get('/users', getUsers);
+
+// get one client
+router.get('/user/:id', getOneUser);
+
+// mark a client as verified or unverified
+router.patch('/user/:id', markUser);
+
+// get current loans
+router.get('/current', getCurrentLoans);
+
+// get repaid loans
+router.get('/repaid', getRepaidLoans);
+
+
+export default router;
